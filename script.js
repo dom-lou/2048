2dd7d327be8b5d36112283a74885e5945630e622
dlouangr <cxe8atj@vjq@MVD*nuc>
2024-02-08 16:47:11 -0500

    Preemptively added id for score and declared them in the js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Declaring values
+const gameBoard = document.getElementById('gameboard');
+const score = document.getElementById('score');
+
+/**
+ * TO-DO:
+ * Method to choose a random square and give it
+ * a value of either 2 or 4.
+ */
