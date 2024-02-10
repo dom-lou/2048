47557ab2d9d995ce3f73fb303b3c12068c819cfd
dlouangr <dlouangr@uncc.edu>
2024-02-10 13:35:48 -0500

    Tiles now render

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 import Grid from "./Grid.js"
+import Tile from "./Tile.js"
 
 // Declaring values
 const gameBoard = document.getElementById("gameboard");
 const score = document.getElementById("score");
 
 const grid = new Grid(gameBoard);
-console.log(grid.randomEmptyCell());
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
