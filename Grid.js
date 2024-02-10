13d2dcbce5f7febcd375868d620706b4608b5b1a
dlouangr <dlouangr@uncc.edu>
2024-02-10 15:19:23 -0500

    Setup move functions; currently nonfunctional

--- Grid.js
+++ Grid.js
@@ -14,6 +14,14 @@ export default class Grid {
         })
     }
 
+    get cellsByColumn () {
+        return this.#cells.reduce((cellGrid, cell) => {
+            cellGrid[cell.x] = cellGrid[cell .x] || []
+            cellGrid[cell.x][cell.y] = cell
+            return cellGrid
+        }, [])
+    }
+
     get #emptyCells() {
         return this.#cells.filter(cell => cell.tile == null)
     }
@@ -25,14 +33,24 @@ export default class Grid {
 }
 
 class Cell {
-    #cellElement
-    #x
-    #y
-    #tile
+    #cellElement;
+    #x;
+    #y;
+    #tile;
+    #mergeTile;
+
     constructor(cellElement, x, y) {
-        this.#cellElement = cellElement
-        this.#x = x
-        this.#y = y
+        this.#cellElement = cellElement;
+        this.#x = x;
+        this.#y = y;
+    }
+
+    get x() {
+        return this.#x;
+    }
+
+    get y() {
+        return this.#y;
     }
 
     get tile() {
@@ -40,20 +58,38 @@ class Cell {
     }
 
     set tile(value) {
-        this.#tile = value
-        if (value == null) return
-        this.#tile.x = this.#x
-        this.#tile.y = this.#y
+        this.#tile = value;
+        if (value == null) return;
+        this.#tile.x = this.#x;
+        this.#tile.y = this.#y;
+    }
+
+    get mergeTile() {
+        return this.#mergeTile;
+    }
+
+    set mergeTile(value) {
+        this.#mergeTile = value;
+        if (value == null) return;
+        this.#mergeTile.x = this.#x;
+        this.#mergeTile.y = this.#y
+    }
+
+    canAccept(tile) {
+        return (
+            this.tile == null || 
+            (this.mergeTile == null && this.tile.value === tile.value)
+        );
     }
 }
 
 function createCellElements(gridElement) {
-    const cells = []
+    const cells = [];
     for (let i = 0; i < gridSize * gridSize; i++) {
-        const cell = document.createElement("div")
-        cell.classList.add("cell")
-        cells.push(cell)
-        gridElement.append(cell)
+        const cell = document.createElement("div");
+        cell.classList.add("cell");
+        cells.push(cell);
+        gridElement.append(cell);
     }
-    return cells
+    return cells;
 }
--- script.js
+++ script.js
@@ -8,3 +8,61 @@ const score = document.getElementById("score");
 const grid = new Grid(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
 grid.randomEmptyCell().tile = new Tile(gameBoard);
+setupInput();
+
+function setupInput() {
+    window.addEventListener("keydown", handleInput, {once: true});
+}
+
+function handleInput(e) {
+    console.log(e.key)
+    switch (e) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+        default:
+            setupInput();
+            break;
+    }
+
+    // Additional code
+
+    setupInput();
+}
+
+function moveUp () {
+    return slideTiles(grid.cellsByColumn);
+}
+
+function slideTiles(cells) {
+    cells.forEach(group => {
+        for (let i = 1; i < group.length; i++) {
+            const cell = group[i];
+            if (cell.tile == null) continue;
+            let lastValidCell = null;
+            for (let j = i-1; j >= 0; j--) {
+                const moveToCell = group[j];
+                if (!moveToCell.canAccept(cell.tile)) break;
+                lastValidCell = moveToCell;
+            }
+            
+            if (lastValidCell != null) {
+                if (lastValidCell.tile != null) {
+                    lastValidCell.mergeTile = cell.tile;
+                } else { // If the movement doesn't merge a number just slide it over
+                    lastValidCell.tile = cell.tile;
+                }
+                cell.tile = null;
+            }
+        }
+    })
+}
