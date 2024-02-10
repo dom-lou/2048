683c8b1d56c277b56a66175eb349df1825b1358c
dlouangr <dlouangr@uncc.edu>
2024-02-10 13:25:44 -0500

    Added functions for working with tiles

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ const gameBoard = document.getElementById("gameboard");
 const score = document.getElementById("score");
 
 const grid = new Grid(gameBoard);
+console.log(grid.randomEmptyCell());
+grid.randomEmptyCell().tile = new Tile(gameBoard);
+grid.randomEmptyCell().tile = new Tile(gameBoard);
